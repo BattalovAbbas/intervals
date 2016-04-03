e0f02ee5c8bb3f5372b49a3be1b704c2ea2ed7d0
BattalovAbbas <battalov.abbas.94@gmail.com>
2016-04-03 12:10:33 +0300

    Update intervals.ino

--- intervals.ino
+++ intervals.ino
@@ -2,8 +2,8 @@
 Servo servo1; 
 Servo servo2; 
 Servo servo3; 
-int distance = 0; 
-float degrees =0; 
+int distance = 0; // Расстояние
+float degrees =0; // Угол
 void setup() 
 { 
 	servo1.attach(13); 
@@ -17,16 +17,16 @@ void setup()
 
 void loop() 
 { 
-	if(Serial.available() > 0) 
+	if(Serial.available() > 0) // Ручной ввод дистанции в дальнейшем через Ультразвуковой датчик измерения расстояния HC-SR04
 	{ 
 		String split = Serial.readString(); 
-		distance = split.toInt()-100; 
+		distance = split.toInt()-100; // Минус 100 м для точности датчика измерения расстояния
 		Serial.println(distance); 
-		if(distance<300) 
+		if(distance<300) // Если расстояние меньше 300мм 
 		{ 
 			servo2.write(0); 
 			servo3.write(0); 
-			degrees = (int)(distance/1.67); 
+			degrees = (int)(distance/1.67); // Рассчет угла наклона
 			servo1.write(degrees); 
 			Serial.println(degrees); 
 			delay(1000); 
