caea6c07d5cdb723aca80e503ae2f607046a49d4
BattalovAbbas <battalov.abbas.94@gmail.com>
2016-04-03 11:56:21 +0300

    Третья подзадача
    Пока что ручной ввод расстояния в дальнейшем через ультразвуковой датчик
    измерения расстояния HC-SR04

--- intervals.ino
+++ intervals.ino
@@ -1,74 +1,54 @@
-int led1 = 13; 
-int led2 = 12; 
-int led3 = 11; 
-int led4 = 10; 
-int led5 = 9; 
-unsigned long interval1 = 200000; 
-unsigned long interval2 = 250000; 
-unsigned long interval3 = 333; 
-unsigned long interval4 = 500; 
-unsigned long interval5 = 1000; 
-int state1 = LOW; 
-int state2 = LOW; 
-int state3 = LOW; 
-int state4 = LOW; 
-int state5 = LOW; 
-unsigned long previousMicros1 = 0; 
-unsigned long previousMicros2 = 0; 
-unsigned long previousMillis3 = 0; 
-unsigned long previousMillis4 = 0; 
-unsigned long previousMillis5 = 0; 
-
+#include <Servo.h> //используем библиотеку для работы с сервоприводом 
+Servo servo1; 
+Servo servo2; 
+Servo servo3; 
+int distance = 0; // Дистанция
+float degrees =0; // Угол наклона
 void setup() 
 { 
-    pinMode(led1, OUTPUT); 
-    pinMode(led2, OUTPUT); 
-    pinMode(led3, OUTPUT); 
-    pinMode(led4, OUTPUT); 
-    pinMode(led5, OUTPUT); 
-    Serial.begin(9600); 
-} 
-
-void function(unsigned long time, int led, unsigned long interval,unsigned long &previous, int &state) 
-{ 
-    if(time - previous > interval) 
-    { 
-        state = !state; 
-        previous = time; 
-        digitalWrite(led, state); 
-    } 
+	servo1.attach(13); 
+	servo2.attach(12); 
+	servo3.attach(11); 
+	Serial.begin(9600); 
+	servo2.write(0); 
+	servo3.write(0); 
+	servo1.write(0); 
 } 
 
 void loop() 
 { 
-    unsigned long timeMillis = millis(); 
-    unsigned long timeMicros = micros(); 
-    function(timeMicros,led1,interval1, previousMicros1,state1); 
-    function(timeMicros,led2,interval2, previousMicros2,state2); 
-    function(timeMillis,led3,interval3, previousMillis3,state3); 
-    function(timeMillis,led4,interval4, previousMillis4,state4); 
-    function(timeMillis,led5,interval5, previousMillis5,state5); 
-    if(Serial.available() > 0) 
-    { 
-        String split = Serial.readString(); 
-        Serial.println("led = " + split.substring(0, 1)+" interval = " + split.substring(2, split.length())); 
-        switch(split.substring(0, 1).toInt()) 
-        { 
-            case 1: 
-            interval1=split.substring(2, split.length()).toInt(); 
-            break; 
-            case 2: 
-            interval2=split.substring(2, split.length()).toInt(); 
-            break; 
-            case 3: 
-            interval3=split.substring(2, split.length()).toInt(); 
-            break; 
-            case 4: 
-            interval4=split.substring(2, split.length()).toInt(); 
-            break; 
-            case 5: 
-            interval5=split.substring(2, split.length()).toInt(); 
-            break; 
-        } 
-    } 
+	if(Serial.available() > 0) // Ручной ввод расстояния, в дальшейшем через Ультразвуковой датчик измерения расстояния
+	{ 
+		String split = Serial.readString(); // Считываем дистанцию введеную дистанцию в мм
+		distance = split.toInt()-100; // Минус 100 мм 
+		Serial.println(distance); 
+		if(distance<300) // Если расстояние меньше 300мм крутим первый серв
+		{ 
+			servo2.write(0); 
+			servo3.write(0); 
+			degrees = (int)(distance/1.67); // Высчитываем угол
+			servo1.write(degrees); 
+			Serial.println(degrees); 
+			delay(1000); 
+		} 
+		else if(distance<600) Если расстояние больше 300 но меньше 600мм крутим второй серв
+		{ 
+			servo1.write(180); 
+			servo3.write(0); 
+			degrees = (int)((distance-300)/1.67); 
+			servo2.write(degrees); 
+			Serial.println(degrees); 
+			delay(1000); 
+		} 
+		else Если расстояние больше 600 крутим третий серв
+		{ 
+			servo1.write(180); 
+			servo2.write(180); 
+			degrees = (int)((distance-600)/1.67); 
+			servo3.write(degrees); 
+			Serial.println(degrees); 
+			delay(1000); 
+		} 
+
+	} 
 }
