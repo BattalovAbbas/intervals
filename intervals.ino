7c2a708feab877cf8b6d6f323bcf5a228f6eea11
BattalovAbbas <battalov.abbas.94@gmail.com>
2016-04-08 16:03:32 +0300

    Импульсы
    Добавлено среднее значение расстояния. 
    Угол сервопривода устанавливается импульсом. 544мкс-0°,2400мкс-180°,1475мкс-90°,1°= 10мкс, 1мм = 5,6мкс, индивидуально для каждого сервопривода.

--- intervals.ino
+++ intervals.ino
@@ -1,30 +1,26 @@
-#include <Servo.h> 
-Servo myservo; 
+
 int echoPin = 9; 
 int trigPin = 8; 
 
-Servo servo1; 
-Servo servo2; 
-Servo servo3; 
-int distance = 0; 
-int degrees =0; 
-int mediumDistance =0;
+int servoPin1 = 13;
+int servoPin2 = 12;
+int servoPin3 = 11;
 
+int distance = 0; 
+int pulse=0;
+int mediumDistance =0;
+int count=0;
 void setup() 
 { 
 	pinMode(trigPin, OUTPUT); 
 	pinMode(echoPin, INPUT); 
 	Serial.begin(9600); 
-
-	servo1.attach(13); 
-	servo2.attach(12); 
-	servo3.attach(11); 
-	servo2.write(0); 
-	servo3.write(10); 
-	servo1.write(10); 
+	pinMode(servoPin1, OUTPUT);
+	pinMode(servoPin2, OUTPUT);
+	pinMode(servoPin3, OUTPUT);
 } 
 
-void distans () 
+void distans() 
 { 
 	float dist; 
 	digitalWrite(trigPin, LOW); 
@@ -32,38 +28,63 @@ void distans ()
 	digitalWrite(trigPin, HIGH); 
 	delayMicroseconds(10); 
 	digitalWrite(trigPin, LOW); 
-	dist = pulseIn(echoPin, HIGH, 2941); 
+	dist = pulseIn(echoPin, HIGH, 2941); // 2941 микросекунд = 1000мм, индивидуально для каждого дальномера
 	distance = (int)dist / 5.82;
 	Serial.print(distance); 
 	Serial.println(" mm"); 
 } 
 void loop() 
 { 
-	distans ();
-	mediumDistance = (mediumDistance + distance)/2;
-	if(mediumDistance<=300)
+	count++; // Количество вызовов метода поиска дистанции
+	distans(); //выход от 0 до 1000 мм
+	mediumDistance = (mediumDistance*(count-1) + distance)/count; // Среденее значение среди всех дистанций
+	if(mediumDistance<333)
 	{ 
-		servo2.write(0); 
-		servo3.write(10); 
-		degrees = (int)(mediumDistance/1.67); 
-		servo1.write(degrees); 
-		Serial.println(degrees); 
+		digitalWrite(servoPin2,HIGH);
+  		delayMicroseconds(544); // 544мкс-0°,2400мкс-180°,1475мкс-90°,1°= 10мкс, 1мм = 5,6мкс, индивидуально для каждого сервопривода
+  		digitalWrite(servoPin2,LOW);
+  		delay(20);
+		digitalWrite(servoPin3,HIGH);
+  		delayMicroseconds(544);
+  		digitalWrite(servoPin3,LOW);
+  		delay(20);
+  		digitalWrite(servoPin1,HIGH);
+  		delayMicroseconds(544+mediumDistance*5.6); // 0 градусов + дистанция*5.6
+  		digitalWrite(servoPin1,LOW);
+  		delay(20);
+  		Serial.println((544+mediumDistance*5.6); 
 	} 
-	else if(mediumDistance<=600) 
+	else if(mediumDistance<666) 
 	{ 
-		servo1.write(180); 
-		servo3.write(10); 
-		degrees = (int)((mediumDistance-300)/1.67); 
-		servo2.write(degrees); 
-		Serial.println(degrees); 
+		digitalWrite(servoPin1,HIGH);
+  		delayMicroseconds(2400); 
+  		digitalWrite(servoPin1,LOW);
+  		delay(20);
+		digitalWrite(servoPin3,HIGH);
+  		delayMicroseconds(544);
+  		digitalWrite(servoPin3,LOW);
+  		delay(20);
+  		digitalWrite(servoPin2,HIGH);
+  		delayMicroseconds(544+(mediumDistance-333)*5.6);
+  		digitalWrite(servoPin2,LOW);
+  		delay(20);
+  		Serial.println((544+(mediumDistance-333)*5.6); 
 	} 
-	else if(mediumDistance<=900) 
+	else if(mediumDistance<1000) 
 	{ 
-		servo1.write(180); 
-		servo2.write(180); 
-		degrees = (int)((mediumDistance-600)/1.67); 
-		servo3.write(degrees); 
-		Serial.println(degrees); 
+		digitalWrite(servoPin1,HIGH);
+  		delayMicroseconds(2400); 
+  		digitalWrite(servoPin1,LOW);
+  		delay(20);
+		digitalWrite(servoPin2,HIGH);
+  		delayMicroseconds(2400);
+  		digitalWrite(servoPin2,LOW);
+  		delay(20);
+  		digitalWrite(servoPin3,HIGH);
+  		delayMicroseconds(544+(mediumDistance-666)*5.6); 
+  		digitalWrite(servoPin3,LOW);
+  		delay(20);
+  		Serial.println((544+(mediumDistance-666)*5.6); 
 	} 
 	delay(100); 
 }
